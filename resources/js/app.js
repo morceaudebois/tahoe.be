626e4bcc2a46b1b2d9356b315ca6201985861a68
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2024-03-02 13:41:08 +0100

    Particle animation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
-import './bootstrap';
+import './bootstrap'
 
 
 // Mouse animation stolen here: https://codepen.io/kevinpowell/pen/GRBdLEv
-const articles = document.querySelectorAll("#projects article");
+const articles = document.querySelectorAll("#projects article")
 
 articles.forEach(function(pre) {
     document.addEventListener("mousemove", (e) => {
@@ -26,9 +26,62 @@ function rotateElement(event, element) {
     // and tone it down a little
     const offsetX = ((x - middleX) / middleX) * 45
     const offsetY = ((y - middleY) / middleY) * 45
-    // console.log(offsetX, offsetY);
+    // console.log(offsetX, offsetY)
 
     // set rotation
     element.style.setProperty("--rotateX", (offsetX/6) + "deg")
     element.style.setProperty("--rotateY", -1 * (offsetY/6) + "deg")
 }
+
+document.querySelectorAll('.likeContainer').forEach(function(likeContainer) {
+    likeContainer.addEventListener('click', function (event) {
+        event.preventDefault()
+        createFirework(event.clientX, event.clientY)
+    })
+})
+
+function createFirework(x, y) {
+    const container = document.getElementById('particle-container')
+
+    for (let i = 0; i < 30; i++) {
+        const particle = document.createElement('div.particle')
+            particle.className = 'particle'
+
+        const angle = Math.random() * Math.PI * 2
+        const velocity = Math.random() * 8 + 2 // Random velocity between 2 and 10
+
+        const dx = Math.cos(angle) * velocity
+        const dy = Math.sin(angle) * velocity
+
+        particle.style.left = x + 'px'
+        particle.style.top = y + 'px'
+
+        container.appendChild(particle)
+
+        setTimeout(() => {
+            particle.remove()
+        }, 400)
+
+        animateParticle(particle, dx, dy)
+    }
+}
+
+function animateParticle(particle, dx, dy) {
+    const startTime = Date.now()
+    function update() {
+        const deltaTime = Date.now() - startTime
+        const progress = Math.min(deltaTime / 400, 1)
+
+        const x = parseFloat(particle.style.left) + dx * progress
+        const y = parseFloat(particle.style.top) + dy * progress
+
+        particle.style.left = x + 'px'
+        particle.style.top = y + 'px'
+
+        if (progress < 1) requestAnimationFrame(update)
+    }
+
+    requestAnimationFrame(update)
+}
+
+
