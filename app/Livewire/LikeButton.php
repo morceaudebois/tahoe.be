a08e055c4107b29458d4376d60027811ac946e52
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2024-03-10 15:43:04 +0100

    Can only be liked once

--- app/Livewire/LikeButton.php
+++ app/Livewire/LikeButton.php
@@ -15,9 +15,20 @@ class LikeButton extends Component {
     }
 
     public function toggleLike() {
-        $this->element->update(
-            ['likes' => $this->element->likes + 1]
-        );
+        if (!$this->isLiked()) {
+            $this->element->update(
+                ['likes' => $this->element->likes + 1]
+            );
+            $this->setLikedState();
+        }
+    }
+
+    public function isLiked() {
+        return session()->get('liked_' . $this->element->id, false);
+    }
+
+    public function setLikedState() {
+        session()->put('liked_' . $this->element->id, true);
     }
 
     public function render() {
