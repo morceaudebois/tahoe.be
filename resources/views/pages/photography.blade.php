546dd2d8a2b32c3ad4cca5bac8b1f34faa6b038e
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2024-03-10 15:58:10 +0100

    Fix photography likes

--- resources/views/pages/photography.blade.php
+++ resources/views/pages/photography.blade.php
@@ -1,18 +1,20 @@
 <x-front-layout class="photography">
     <section id='photo-grid'>
-        <div class="photoContainer">
+        
             @foreach ($photos->where('draft', false)->sortBy('date') as $photo)
-                <a class="glassHover" href="{{ $photo->getThumbnailUrl('xl') }}" 
-                    data-pswp-width="{{ $photo->width }}" 
-                    data-pswp-height="{{ $photo->height }}" 
-                    target="_blank">
-                    <img src="{{ $photo->getThumbnailUrl('md') }}" alt="" style="aspect-ratio: {{ $photo->width }}/{{ $photo->height }}"/>
-                </a>
-                 <div class="links">
-                    @livewire('like-button', ['element' => $photo, 'glass' => true])
+                <div class="photoContainer glassHover">
+                    <a href="{{ $photo->getThumbnailUrl('xl') }}" 
+                        data-pswp-width="{{ $photo->width }}" 
+                        data-pswp-height="{{ $photo->height }}" 
+                        target="_blank">
+                        <img src="{{ $photo->getThumbnailUrl('md') }}" alt="" style="aspect-ratio: {{ $photo->width }}/{{ $photo->height }}"/>
+                    </a>
+                    <div class="links">
+                        @livewire('like-button', ['element' => $photo, 'glass' => true])
+                    </div>
                 </div>
             @endforeach
-        </div>
+        
 
     </section>
 
