b5cc6e17b065c3414611e91682edef53ce9ea266
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2024-03-12 23:06:04 +0100

    Fix double Alpine

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -25,7 +25,7 @@
 
         @include('front.footer')
 
-        <svg id="bg-glow" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" viewBox="0 0 800 800" opacity="0.18"><defs><filter id="bbblurry-filter" x="-100%" y="-100%" width="400%" height="400%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <svg id="bg-glow" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" viewBox="0 0 800 800"><defs><filter id="bbblurry-filter" x="-100%" y="-100%" width="400%" height="400%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
         <feGaussianBlur stdDeviation="130" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" edgeMode="none" result="blur"></feGaussianBlur></filter></defs><g filter="url(#bbblurry-filter)"><ellipse rx="189.5" ry="277.5" cx="387.37898932969586" cy="389.59564847750903" fill="#e73c7e"></ellipse></g></svg>
 
         <div id="bg-grain"></div>
@@ -39,12 +39,7 @@
             </a>
         @endif
 
-                @livewireScripts
+        @livewireScripts
         @vite(['resources/js/app.js'])
-
-
-        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-        
-
     </body>
 </html>
