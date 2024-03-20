5ab57c5fe196fad99a22d2442ca99ac897f87cb8
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2024-03-20 19:32:32 +0100

    admin.js dans vite

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/scss/front.scss',
                 'resources/js/app.js',
+                'resources/js/admin.js',
             ],
             refresh: true,
         }),
