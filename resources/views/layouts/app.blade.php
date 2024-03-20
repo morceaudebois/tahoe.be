81fab97a7ac1c77185327e64400d88f271297379
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2024-03-20 19:42:00 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/bootstrap.js'])
+        @vite(['resources/css/app.css', 'resources/js/admin.js'])
 
         <!-- Styles -->
         @livewireStyles
