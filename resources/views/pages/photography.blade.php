af375784c680c06a0481687273093e7a5deb5b34
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2024-03-30 14:39:59 +0100

    Blur for cope

--- resources/views/pages/photography.blade.php
+++ resources/views/pages/photography.blade.php
@@ -10,31 +10,33 @@
 
     <section id='photo-grid'>
         @foreach ($photos->where('draft', false)->sortByDesc('date') as $photo)
-            @if (!Auth::check() || Auth::check() && !$photo->cope) 
-                <div class="photo-wrapper">
-                    <div class="photo glassHover">
-                        <a href="{{ $photo->getThumbnailUrl('xl') }}" 
-                            data-pswp-width="{{ $photo->width }}" 
-                            data-pswp-height="{{ $photo->height }}" 
-                            target="_blank"
-                            @if ($photo->info)
-                                data-description="{{ $photo->info }}"
-                            @endif>
-                            <img 
-                                src="{{ $photo->getThumbnailUrl('md') }}" 
-                                alt="{{ $photo->info }}" 
-                                style="aspect-ratio: {{ $photo->width }}/{{ $photo->height }}"
-                                @if ($loop && $loop->index > 6)
-                                    loading="lazy"
+            <div class="photo-wrapper">
+                <div class="photo glassHover">
+                    <a href="{{ $photo->getThumbnailUrl('xl') }}" 
+                        data-pswp-width="{{ $photo->width }}" 
+                        data-pswp-height="{{ $photo->height }}" 
+                        target="_blank"
+                        @if ($photo->info)
+                            data-description="{{ $photo->info }}"
+                        @endif>
+                        <img 
+                            src="{{ $photo->getThumbnailUrl('md') }}" 
+                            alt="{{ $photo->info }}" 
+                            style="aspect-ratio: {{ $photo->width }}/{{ $photo->height }};
+                                @if (Auth::check() && $photo->cope)
+                                    filter: blur(25px)
                                 @endif
-                            />
-                        </a>
-                        <div class="links">
-                            @livewire('like-button', ['element' => $photo, 'glass' => true])
-                        </div>
+                                "
+                            @if ($loop && $loop->index > 6)
+                                loading="lazy"
+                            @endif
+                        />
+                    </a>
+                    <div class="links">
+                        @livewire('like-button', ['element' => $photo, 'glass' => true])
                     </div>
                 </div>
-            @endif
+            </div>
         @endforeach
     </section>
 </x-front-layout>
